de588a53bbe18e58b547fe78824eae14075d55bb
Tobias Kahse <tobias.kahse@netlight.com>
2021-04-14 13:58:16 +0200

    fixed linter errors

--- fastify/src/index.ts
+++ fastify/src/index.ts
@@ -4,15 +4,15 @@ import bundles from './bundles';
 
 const server = fastify({ logger: true });
 
-server.register(bundles)
+server.register(bundles);
 
 const init = async () => {
-	try {
-		await server.listen(3000, 'localhost');
-	} catch (err) {
-		server.log.error(err);
-		process.exit(1);
-	}
+    try {
+        await server.listen(3000, 'localhost');
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
 };
 
 init();
